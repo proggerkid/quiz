b2e7d7f59e20805a3f954d6380637125b3a911c2
proggerkid <proggerkid@yahoo.com>
2021-07-09 17:00:16 +0200

    new

--- controller/ctr_registration.js
+++ controller/ctr_registration.js
@@ -1,37 +1,33 @@
 let questions = require('../questions');
 let model_quiz = require('../model/model_quiz');
 
+async function renderIndexOrQuizCreation(name, res){
+    let questionNumber = await model_quiz.returnQuestionNumber(name);
+    if(questionNumber >= 4){
+        let questionAnsweres = await model_quiz.getAnsweres(name);
+        let id = await model_quiz.getID(name);
+        res.render('index', {
+            answeres: questionAnsweres,
+            userID: id
+        })
+    }
+    else{
+        res.render('quizCreation', {
+            question: questions[questionNumber]
+        })
+    }
+}
+
 module.exports = {
     renderRegistration: (req, res)=>{
+        //if there is no cookie for identifying the user, render registration
         if(!req.cookies.name){
             res.render('registration');
         }
+        //if all questions answered, render index or render quizCreation
         else{
-            let name = req.cookies.name;
-            
-            model_quiz.returnQuestionNumber(name).then((result)=>{
-                let questionNumber = result;
-                
-                if(questionNumber >= 4){
-                    model_quiz.getAnsweres(name).then((result)=>{
-                        let questionAnsweres = result; 
-                        
-                        model_quiz.getID(name).then((result)=>{
-                            let id = result;
-
-                            res.render('index', {
-                                answeres: questionAnsweres,
-                                userID: id  
-                            }) 
-                        })
-                    });
-                }
-                else{
-                    res.render('quizCreation', {
-                        question: questions[questionNumber]
-                    });
-                }
-            });
+            name = req.cookies.name;
+            renderIndexOrQuizCreation(name, res)
         }
     },
     registrateUser: (req, res)=>{
@@ -44,3 +40,5 @@ module.exports = {
         });
     }
 };
+
+
