d2f7e1a9d6bd47236dd7a12a68f3f24dc10994c4
unavailablename <sieber.thomas@yahoo.com>
2021-07-09 15:58:51 +0200

    new

--- controller/ctr_index.js
+++ controller/ctr_index.js
@@ -1,8 +1,22 @@
+model_quiz = require('../model/model_quiz')
+
 module.exports = {
     renderIndex: (req, res)=>{
-        res.render('index', {
-            answeres: [],
-            userID: ""
-        });
+        name = req.cookies.name
+
+        if(name){
+            model_quiz.getID(name).then((id)=>{
+                res.render('index', {
+                    answeres: [],
+                    userID: id
+                })
+            })
+        }
+        else{
+            res.render('index', {
+                answeres: [],
+                userID: ""
+            });
+        }
     }
 }
